0bdebcd223f476bef9461c9cd1e4cf8029a2f4c7
Aasta-Chen <84763551+Aasta-Chen@users.noreply.github.com>
2021-06-08 00:44:03 +0800

    建立 login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+          const url = 'https://vue3-course-api.hexschool.io/';
+          const path = 'aasta-hexschool';
+
+          const usernameInput = document.querySelector('#username')
+          const passwordInput = document.querySelector('#password')
+          const loginBtn = document.querySelector('#login')
+          loginBtn.addEventListener('click', login);  
